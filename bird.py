21aadaee312a2316ce1e87e6826e04157c4d12b4
aizasuzila <115996466+aizasuzila@users.noreply.github.com>
2022-10-18 21:17:23 +0800

    Update bird.py

--- bird.py
+++ bird.py
@@ -2,32 +2,6 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-from IPython.core.pylabtools import figsize
-
-import folium
-from folium.plugins import HeatMap
-import plotly.express as px
-import seaborn as sns
-
-from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
-from sklearn.decomposition import PCA
-
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.svm import SVC
-
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import classification_report
-from sklearn.metrics import plot_confusion_matrix
-
-from sklearn.metrics import mean_squared_error
-from sklearn.metrics import mean_absolute_error
-from sklearn.metrics import mean_squared_error
-from sklearn.metrics import mean_absolute_percentage_error
-from sklearn.metrics import r2_score
 
 st.write(data=pd.read_csv('/content/bird.csv'))
 
