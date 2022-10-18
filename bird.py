236209d8fffa290bf34334a7595c47ecdc476070
aizasuzila <115996466+aizasuzila@users.noreply.github.com>
2022-10-18 21:28:10 +0800

    Update bird.py

--- bird.py
+++ bird.py
@@ -1,14 +1,45 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from IPython.core.pylabtools import figsize
 
-st.write(data=pd.read_csv('/content/bird.csv'))
+import folium
+from folium.plugins import HeatMap
+import plotly.express as px
+import seaborn as sns
 
-st.write(data)
 
-st.write(dcopy=data.copy())
+from sklearn.preprocessing import LabelEncoder
 
-st.write(dcopy.shape)
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.decomposition import PCA
 
-st.write(dcopy.columns)
 
-st.write(dcopy.dtypes)
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+from sklearn.metrics import plot_confusion_matrix
+
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_percentage_error
+from sklearn.metrics import r2_score
+
+data=pd.read_csv('/content/bird.csv')
+
+data
+
+dcopy=data.copy()
+
+dcopy.shape
+
+dcopy.columns
+
+dcopy.dtypes
